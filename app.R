6423e724a18a64f9ca7ea1fb688a6960c8ad5a16
rgottberg <rgottberg@gmail.com>
2025-03-14 12:22:54 +0100

    button & reactive environment

--- app.R
+++ app.R
@@ -1,8 +1,22 @@
 # install libraries
-install.packages("shiny")
-install.packages("ggplot2")
-install.packages("dplyr")
-install.packages("tidyr")
+# install.packages("shiny")
+# install.packages("dplyr")
+# install.packages("tidyr")
+# install.packages("bslib")
+
+
+# Load libraries
+library(dplyr)
+library(tibble)
+
+library(shiny)
+library(bslib)
+library(shinythemes)
+
+library(shinyWidgets)
+library(shinyFeedback)
+
+
 
 # load data
 data(iris)
@@ -21,9 +35,12 @@ ui <-
         sidebar = sidebar(
         # FARIBA: filter1: select > choose species
         # ARPAD: filter2: slider > sepal.length
-        # RUBEN: button: select random species
-      ),
-      nav_panel(title = "",
+        
+          #Choose a random specie
+          actionButton("random_select",
+                       "Choose a random species")
+        ),
+        nav_panel(title = "",
                 layout_columns(
                   col_widths = c(6,6),
                   #FARIBA > CHART W/ PLOTLY 
@@ -49,17 +66,29 @@ ui <-
                     card_header(""),
                     full_screen = T,
                     card_body(dataTableOutput("") ),
+                    ),
                   ),
                 ),
-      ),
-      nav_spacer(),
-      nav_item(input_dark_mode(id = "dark_mode", mode = "light")),
-    ),
+        nav_spacer(),
+        nav_item(input_dark_mode(id = "dark_mode", mode = "light")),
+        )
   )
 # Define server logic -----------------------------------------------------
 server <- function(input, output) {
   
   #EACH TEAM MEMBER TO WRITE THE SERVER LOGIC
+  
+  #RUBEN
+  # eventReactive() to choose a random specie when pressing an input button
+  data_filtered <- eventReactive(input$random_select,{
+    iris |>
+      dplyr::filter(Species == sample(Species,1))
+  })
+  # Use observe() to log changes in the console.
+  observe({
+    print(paste("User has clicked on the button",input$random_select,"time(s)"))
+  })
+  
 }
 # Run the application -----------------------------------------------------
 shinyApp(ui, server, options = list())
